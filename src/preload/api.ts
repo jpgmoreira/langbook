fa0f877621fc396cfd2a7ea231fd9d17e13f803b
joao <jpgmoreira19@gmail.com>
2025-11-08 14:58:00 -0300

    configured preload

---
+++ src/preload/api.ts
@@ -0,0 +1,11 @@
+import { Channels } from './channels';
+
+export const allowedSendChannels = Object.freeze([] as const);
+export const allowedInvokeChannels = Object.freeze([] as const);
+export const allowedOnChannels = Object.freeze([Channels.loadStartupData] as const);
+
+export interface ElectronAPI {
+  send: (channel: (typeof allowedSendChannels)[number], ...data: any[]) => void;
+  invoke: <T = void>(channel: (typeof allowedInvokeChannels)[number], ...data: any[]) => Promise<T>;
+  on: (channel: (typeof allowedOnChannels)[number], func: (...args: any[]) => void) => void;
+}
