84fd9c9c07b9e6bcb003fe8d3e64a55113700611
joao <jpgmoreira19@gmail.com>
2025-11-08 23:58:48 -0300

    added header to home

---
+++ src/renderer/components/Header.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header class="nav-header flex justify-between">
+    <RouterLink to="/home">Home</RouterLink>
+    <div class="flex">
+      <RouterLink to="/graph">Graph</RouterLink>
+      <RouterLink to="/settings">Settings</RouterLink>
+    </div>
+  </header>
+</template>
--- src/renderer/pages/HomePage.vue
+++ src/renderer/pages/HomePage.vue
@@ -1,1 +1,9 @@
-<template>Home!</template>
+<script lang="ts" setup>
+  import Header from '@renderer/components/Header.vue';
+</script>
+
+<template>
+  <div class="h-screen" style="border: 1px solid red">
+    <Header />
+  </div>
+</template>
