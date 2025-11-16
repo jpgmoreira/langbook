4a2fbe6ffad98b133772e50bacadadd0b7a51e2f
joao <jpgmoreira19@gmail.com>
2025-11-15 21:53:15 -0300

    pasting images

--- src/renderer/components/UI/RichTextEditor/RichTextEditor.vue
+++ src/renderer/components/UI/RichTextEditor/RichTextEditor.vue
@@ -1,4 +1,46 @@
 <script lang="ts" setup>
+  import { stripHtml } from 'string-strip-html';
+
+  function paste(e: ClipboardEvent) {
+    e.preventDefault();
+    if (!e.clipboardData) return;
+    // 1. Check if pasting an image from the clipboard:
+    const items = e.clipboardData.items;
+    if (items && items.length && items[0].kind === 'file' && items[0].type.startsWith('image/')) {
+      const file = items[0].getAsFile();
+      if (!file) return;
+      console.log('-> paste an image from the clipboard.');
+      const reader = new FileReader();
+      reader.onload = (fileEvent) => {
+        const result = fileEvent.target?.result;
+        if (typeof result !== 'string') return;
+        document.execCommand('insertImage', false, result);
+      };
+      reader.readAsDataURL(file);
+      return;
+    }
+    // 2. Check if pasting HTML content.
+    // No HTML content other than images is allowed.
+    // It's possible to paste HTML tags as plaintext, but if there is an image tag in the content, then other HTML tags will be stripped.
+    const html = e.clipboardData.getData('text/html').trim();
+    if (html && html.includes('<img ')) {
+      console.log('-> paste as HTML.');
+      const stripped = stripHtml(html, { ignoreTags: ['img'] }).result;
+      setTimeout(() => {
+        document.execCommand('insertHTML', false, stripped);
+      }, 0);
+      return;
+    }
+    // 3. Paste as plaintext:
+    const text = e.clipboardData.getData('text/plain').trim();
+    if (text) {
+      console.log('-> paste as plaintext');
+      setTimeout(() => {
+        document.execCommand('insertText', false, text);
+      }, 0);
+    }
+  }
+
   function keydown(e: KeyboardEvent) {
     // Allowed keyboard hotkeys:
     // - CTRL + c: Copy.
@@ -44,5 +86,5 @@
 </script>
 
 <template>
-  <div spellcheck="false" contenteditable="true" @keydown="keydown"></div>
+  <div spellcheck="false" contenteditable="true" @keydown="keydown" @paste="paste"></div>
 </template>
