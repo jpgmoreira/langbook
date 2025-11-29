4694ebc7f3edbd964727121bd6f80208ce532d11
joao <jpgmoreira19@gmail.com>
2025-11-29 17:07:29 -0300

    adjusting context menu on cardsview

--- src/renderer/components/CardsView.vue
+++ src/renderer/components/CardsView.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+  import { reactive, computed, useTemplateRef, onMounted, onBeforeUnmount } from 'vue';
   import type { Card, MediaFile } from '@common/schemas/card';
   import HomeCard from './HomeCard.vue';
   import { parseTimestamp } from '@common/utils/dateUtils';
@@ -8,12 +9,47 @@
     height: number;
     onMediaClick: (media: MediaFile) => void;
   }>();
+  const contextMenu = reactive({
+    visible: false,
+    x: 0,
+    y: 0,
+    card: null as Card | null,
+  });
+  const contextStyle = computed(() => ({
+    left: `${contextMenu.x}px`,
+    top: `${contextMenu.y}px`,
+  }));
+  const rootRef = useTemplateRef('root');
+  let observer: ResizeObserver | null = null;
+  function showContextMenu(e: MouseEvent, card: Card) {
+    const distanceToRight = window.innerWidth - e.clientX;
+    const MENU_WIDTH = 100;
+    if (distanceToRight < MENU_WIDTH) {
+      contextMenu.x = window.innerWidth - distanceToRight - MENU_WIDTH;
+    } else {
+      contextMenu.x = e.clientX;
+    }
+    contextMenu.y = e.clientY;
+    contextMenu.card = card;
+    contextMenu.visible = true;
+  }
+  function hideContextMenu() {
+    contextMenu.visible = false;
+  }
+  onMounted(() => {
+    observer = new ResizeObserver(hideContextMenu);
+    if (rootRef.value) observer.observe(rootRef.value);
+  });
+  onBeforeUnmount(() => {
+    observer?.disconnect();
+  });
 </script>
 
 <template>
-  <div class="cards-view h-full relative">
+  <div class="cards-view h-full relative" @click="hideContextMenu" ref="root">
     <div v-if="!props.page.length" class="text-xl opacity-70 absolute-center">No cards to show</div>
-    <div v-else class="overflow-auto relative h-full">
+    <div v-else class="overflow-auto relative h-full" @scroll="hideContextMenu">
+      <div class="context-menu fixed" v-if="contextMenu.visible" :style="contextStyle">AAAA</div>
       <div
         class="absolute top-0 left-0 bottom-0 w-full flex flex-col"
         :style="{ height: '3000px' }"
@@ -23,7 +59,11 @@
             <span>{{ index + anchor + 1 }}</span>
             <span>Created: {{ parseTimestamp(card.createdAt) }}</span>
           </div>
-          <HomeCard :card="card" :on-media-click="onMediaClick" />
+          <HomeCard
+            :card="card"
+            :on-media-click="onMediaClick"
+            @mousedown.right="showContextMenu($event, card)"
+          />
         </div>
       </div>
     </div>
