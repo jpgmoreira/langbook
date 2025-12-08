db5408d0921bdecd8ac217401adc4ee8755b63b9
joao <jpgmoreira19@gmail.com>
2025-12-08 20:09:06 -0300

    settings page finished

--- src/renderer/pages/SettingsPage.vue
+++ src/renderer/pages/SettingsPage.vue
@@ -26,9 +26,9 @@
         reviewProb.value / 100,
         suspendedProb.value / 100
       );
-      uiStore.showToast('Probabilities updated!', 'success');
+      uiStore.showToast('Probabilities updated!', 'success', 1000);
     } catch {
-      uiStore.showToast('Failed to update probabilities!', 'error');
+      uiStore.showToast('Failed to update probabilities!', 'error', 1000);
     }
   }
   function resetProbs() {
@@ -48,41 +48,58 @@
 <template>
   <div v-if="profileStore.currProfile" class="settings-page w-[100vw] h-[100vh] flex flex-col">
     <Header />
-    <div>
-      <div>Flashcards probabilities:</div>
-      <div class="w-fit">
-        <div class="flex items-center gap-1 justify-end">
-          <label for="review-prob">Review (%):</label>
-          <input name="review-prob" id="review-prob" type="number" v-model="reviewProb" />
-        </div>
-        <div class="flex items-center gap-1">
-          <label for="suspended-prob">Suspended (%):</label>
-          <input name="suspended-prob" id="suspended-prob" type="number" v-model="suspendedProb" />
-        </div>
-      </div>
-      <div v-if="reviewProb + suspendedProb > 100" class="text-danger">
-        The sum of the probabilities should be less than or equal 100%.
-      </div>
+    <div class="p-8">
       <div>
-        <button type="button" class="btn-primary" @click="updateProbs">Save</button>
-        <button type="button" class="btn-primary" @click="resetProbs">Reset</button>
+        <div class="section-title">Flashcards probabilities:</div>
+
+        <div class="w-fit">
+          <div class="input-row">
+            <label for="review-prob" class="whitespace-nowrap">Review (%):</label>
+            <input id="review-prob" type="number" v-model="reviewProb" />
+          </div>
+
+          <div class="input-row">
+            <label for="suspended-prob">Suspended (%):</label>
+            <input id="suspended-prob" type="number" v-model="suspendedProb" />
+          </div>
+        </div>
+
+        <div v-if="reviewProb + suspendedProb > 100" class="text-danger">
+          The sum of the probabilities should be less than or equal 100%.
+        </div>
+
+        <div class="button-row">
+          <button type="button" class="btn-primary" @click="updateProbs">Save</button>
+          <button type="button" class="btn-primary" @click="resetProbs">Reset</button>
+        </div>
       </div>
-    </div>
-    <hr />
-    <div>
-      <div>Profile</div>
-      <div class="flex justify-between">
-        <div>Name: {{ profileStore.currProfile!.name }}</div>
-        <button type="button" class="btn-warning" @click="logout">Logout</button>
-      </div>
-    </div>
-    <hr />
-    <div>
-      <div>App</div>
-      <div>{{ APP_NAME }} &ndash; Version {{ APP_VERSION }}</div>
+
+      <hr />
+
       <div>
-        Homepage:
-        <a href="#" @click="copyHomepage">{{ APP_HOMEPAGE }}</a>
+        <div class="section-title">Profile</div>
+
+        <div class="profile-box">
+          <div>
+            Name:
+            <span class="font-bold">{{ profileStore.currProfile!.name }}</span>
+          </div>
+          <button type="button" class="btn-warning" @click="logout">Logout</button>
+        </div>
+      </div>
+
+      <hr />
+
+      <div>
+        <div class="section-title">App</div>
+
+        <div class="app-info">
+          <div>{{ APP_NAME }} – Version {{ APP_VERSION }}</div>
+          <div>
+            Homepage:
+            <a href="#" @click="copyHomepage">{{ APP_HOMEPAGE }}</a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
